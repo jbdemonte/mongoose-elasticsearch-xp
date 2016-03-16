a8ecbe13ab4220a0e6052e8f7de53fa3b0a62098
Jean-Baptiste Demonte <jbdemonte@gmail.com>
2016-03-16 05:41:53 +0100

    Test #1

--- test/esIndex.js
+++ test/esIndex.js
@@ -51,4 +51,56 @@ describe("esIndex", function () {
       });
   });
 
+  it('should index ObjectId from object populated or not', function (done) {
+
+    var CountrySchema = new mongoose.Schema({
+      name: String
+    });
+
+    var CitySchema = new mongoose.Schema({
+      name: String
+    });
+
+    var UserSchema = new mongoose.Schema({
+      name: String,
+      city: {type: mongoose.Schema.Types.ObjectId, ref: 'City'},
+      country: {type: mongoose.Schema.Types.ObjectId, ref: 'Country'}
+    });
+
+    UserSchema.plugin(plugin);
+
+    var UserModel = mongoose.model('User', UserSchema);
+    var CityModel = mongoose.model('City', CitySchema);
+    var CountryModel = mongoose.model('Country', CountrySchema);
+
+    var country = new CountryModel({name: 'France'});
+    var city = new CityModel({name: 'Paris'});
+    var john = new UserModel({name: 'John', city: city, country: country._id});
+
+    utils.deleteModelIndexes(UserModel)
+      .then(function () {
+        return UserModel.esCreateMapping();
+      })
+      .then(function () {
+        return john.esIndex();
+      })
+      .then(function () {
+        return UserModel.esRefresh();
+      })
+      .then(function () {
+        var options = UserModel.esOptions();
+        var client = options.client;
+        client.search({index: options.index, type: options.type, body: {query: {match_all: {}}}}, function (err, resp) {
+          expect(resp.hits.total).to.eql(1);
+          var hit = resp.hits.hits[0];
+          expect(hit._id).to.eql(john._id.toString());
+          expect(hit._source).to.eql({name: 'John', city: city.id, country: country.id});
+          done();
+        });
+      })
+      .catch(function (err) {
+        done(err);
+      });
+  });
+
 });
