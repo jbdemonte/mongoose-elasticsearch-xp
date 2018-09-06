09216b4be313bb13d07e067c09412196cca1eed2
Borodayev Valeriy <borodaev97@gmail.com>
2018-09-06 16:48:18 +0600

    feat: add mongoose discriminators support (#47, thanks @FrankAst )
    * wip
    
    * test: add tests for `withDiscriminators` case
    
    * refactor(index): small fixes
    
    * refactor: define types as a function only
    
    * refactor: fix test, generate mapping if discriminators are enabled
    
    * refactor: add cachedMappings
    
    * docs: update README.md
    
    * ci: change node 9 => 10

---
+++ test/es5/withDiscriminators.js
@@ -0,0 +1,111 @@
+'use strict';
+
+const utils = require('../utils');
+const mongoose = require('mongoose');
+const plugin = require('../../');
+
+describe('with discriminators', () => {
+  utils.setup();
+
+  it('check types and mappings', () => {
+    const BaseSchema = new mongoose.Schema({
+      name: String,
+    });
+
+    const UserSchema = new mongoose.Schema({
+      age: Number,
+    });
+
+    const AdminSchema = new mongoose.Schema({
+      access: Boolean,
+    });
+
+    BaseSchema.plugin(plugin, {
+      index: 'user',
+      type: kind => {
+        if (kind === 'User') return 'userType';
+        if (kind === 'Admin') return 'adminType';
+        return 'otherType';
+      },
+    });
+
+    const BaseModel = mongoose.model('Base', BaseSchema);
+    const UserModel = BaseModel.discriminator('User', UserSchema);
+    const AdminModel = BaseModel.discriminator('Admin', AdminSchema);
+
+    // check types and mappings on models
+    const userOpts = UserModel.esOptions();
+    const adminOpts = AdminModel.esOptions();
+    expect(userOpts.type).to.equal('userType');
+    expect(adminOpts.type).to.equal('adminType');
+    expect(userOpts.mapping).to.deep.equal({
+      properties: {
+        __t: {
+          type: 'text',
+        },
+        age: {
+          type: 'double',
+        },
+        name: {
+          type: 'text',
+        },
+      },
+    });
+    expect(adminOpts.mapping).to.deep.equal({
+      properties: {
+        __t: {
+          type: 'text',
+        },
+        access: {
+          type: 'boolean',
+        },
+        name: {
+          type: 'text',
+        },
+      },
+    });
+
+    // check types and mappings on docs
+    UserModel.create({
+      name: 'John',
+      age: 34,
+    }).then(doc => {
+      const opts = doc.esOptions();
+      expect(opts.type).to.equal('userType');
+      expect(opts.mapping).to.deep.equal({
+        properties: {
+          __t: {
+            type: 'text',
+          },
+          age: {
+            type: 'double',
+          },
+          name: {
+            type: 'text',
+          },
+        },
+      });
+    });
+
+    AdminModel.create({
+      name: 'Steve',
+      access: true,
+    }).then(doc => {
+      const opts = doc.esOptions();
+      expect(opts.type).to.equal('adminType');
+      expect(opts.mapping).to.deep.equal({
+        properties: {
+          __t: {
+            type: 'text',
+          },
+          access: {
+            type: 'boolean',
+          },
+          name: {
+            type: 'text',
+          },
+        },
+      });
+    });
+  });
+});
